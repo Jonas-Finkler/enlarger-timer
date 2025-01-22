9ab770425270d730434d04a778e71564ac38cdf1
Jonas <git@finkler.me>
2025-01-22 21:26:04 +0100

    Add left button, finalize pins

--- remote/src/main.cpp
+++ remote/src/main.cpp
@@ -5,11 +5,13 @@
 #include <TM1637Display.h>
 
 
-#define ENCODER_SW_PIN D7
-#define ENCODER_DT_PIN D6
-#define ENCODER_CLK_PIN D5
+#define ENCODER_SW_PIN D6
+#define ENCODER_DT_PIN D5
+#define ENCODER_CLK_PIN D4
 #define LED_DIO D1
 #define LED_CLK D2
+#define BUTTON_L_PIN D3
+#define BUTTON_R_PIN D7 
 
 const uint8_t SEG_ON[] = {
   0,
@@ -31,14 +33,23 @@ const uint8_t SEG_CONN[] = {
 	};
 TM1637Display ledDisplay(LED_CLK, LED_DIO);
 
+
+// TODO: 
+// - the left button should start a 30s timer without turning on the lamp
+// - when held donw, the left button should allow to set the blind timer
+
  
+volatile bool leftButtonWasPressed = false;
+void leftButtonPressed() { leftButtonWasPressed = true; }
+Button leftButton(BUTTON_L_PIN, leftButtonPressed);
+
 volatile bool encoderButtonWasPressed = false;
 void encoderButtonPressed() { encoderButtonWasPressed = true; }
 Button encoderButton(ENCODER_SW_PIN, encoderButtonPressed);
 
 volatile bool buttonWasPressed = false;
 void buttonPressed() { buttonWasPressed = true; }
-Button button(D3, buttonPressed);
+Button button(BUTTON_R_PIN, buttonPressed);
 
 Encoder encoder(ENCODER_DT_PIN, ENCODER_CLK_PIN);
 unsigned long lastEncoderChange = 0;
@@ -56,6 +67,7 @@ unsigned long lastSwitchChange = 0;
 bool switchState = false;
 
 bool onMode = false;
+bool fastMode = false;
 
 
 void updateEncoder() {
@@ -71,10 +83,17 @@ void updateEncoder() {
   /*encoderRefValue = min(encoderValue, encoderRefValue);*/
   int encoderDelta = encoderValue - encoderRefValue;
   int setTimeDelta = (encoderDelta + 2 + 40) / 4 - 10; // adding the 40 to always round down 
+  if (fastMode) {
+    setTimeDelta *= 5;
+  }
   setTime = setTimeDelta + encoderAccum ; // in 1/10 s
 
   // NOTE: implements: setTime = max(-1, setTime); but avoids hidden negative encoderDelta
-  encoderAccum = max(-1 * setTimeDelta - 1, encoderAccum);
+  if (fastMode) {
+    encoderAccum = max(-1 * setTimeDelta - 5, encoderAccum);
+  } else {
+    encoderAccum = max(-1 * setTimeDelta - 1, encoderAccum);
+  }
 
   // adjust reference, in case encoder gets out of alignment (4 per click)
   if ((millis() - lastEncoderChange > 500) && !timerRunning && encoderDelta != 0) {
@@ -104,16 +123,21 @@ void loop() {
 
   if (buttonWasPressed) {
     buttonWasPressed = false;
-    if (setTime > 0 && !timerRunning && !onMode) {
-      timerDuration = setTime * 100;
-      setTimer(timerDuration);
-      timerStart = millis();
-      timerRunning = true;
+    if (timerRunning) { // cancel timer
+      timerRunning = false;
+      setState(false);
+    } else {
+      if (setTime > 0 && !onMode) {
+        timerDuration = setTime * 100;
+        setTimer(timerDuration);
+        timerStart = millis();
+        timerRunning = true;
+      }
     }
   }
 
-  if (encoderButtonWasPressed) {
-    encoderButtonWasPressed = false;
+  if (leftButtonWasPressed) {
+    leftButtonWasPressed = false;
     if (!timerRunning) {
       if (setTime < 0) { // reset to 0, turn off
         encoderAccum = 0;
@@ -126,7 +150,14 @@ void loop() {
         }
         onMode = !onMode;
       }
-    }
+    } 
+  }
+
+  if (encoderButtonWasPressed) {
+    encoderButtonWasPressed = false;
+    fastMode = !fastMode;
+    encoderAccum = setTime;
+    encoderRefValue = encoder.read();
   }
 
   bool shouldBeOn = setTime < 0 || onMode;
